1262cc71e43804fe34fa418c9843e79959e1b7b1
sheplu <jean.burellier@gmail.com>
2019-08-21 08:17:57 +0200

    express and webpack

---
+++ express3/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+const { NODE_ENV = 'production', } = process.env;
+
+  module.exports = {
+    entry: './src/index.ts',
+    mode: NODE_ENV,
+    target: 'node',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new WebpackShellPlugin({
+          onBuildEnd: ['yarn run:dev']
+        })
+    ],
+    watch: NODE_ENV === 'development',
+    externals: [ nodeExternals() ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                'ts-loader',
+                ]
+            }
+        ]
+    }
+  }
